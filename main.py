5ed3a9dd686746a3ccf184c49c11401bc78d5c78
ed <edgarcostilla@gmail.com>
2026-08-13 18:36:55 +0200

    Set up FastAPI with ROI router

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from fastapi import FastAPI
+from adapters.entrypoints.api import router as roi_router
 
+app = FastAPI(title="Real Estate ROI Engine")
+
+app.include_router(roi_router, prefix="/v1")
